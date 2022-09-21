58f1fc4fd864ac4213a1130bf820ac6123955abf
Anthony Shellman <atshellman@gmail.com>
2022-09-21 17:28:53 -0600

    Initial commit

---
+++ src/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+const Nav = () => {
+  return (
+    <header>
+      <div className="branding">
+        <h1>Uplift.me</h1>
+      </div>
+      <div className="view-button-container">
+        <button>Placeholder</button>
+      </div>
+    </header>
+  )
+}
+
+
+export default Nav
