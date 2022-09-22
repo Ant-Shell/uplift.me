e3ab08008e19a764d45140815eac7b3aeb8156ba
Anthony Shellman <atshellman@gmail.com>
2022-09-22 13:20:58 -0600

    Add imports, componentDidMount function

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Nav from "../Nav/Nav"
 import './App.css';
+import { quotesEndpoint, animalEndpoint } from "../endpoints"
+import fetchData from "../apiCalls";
+
 
 class App extends React.Component {
   constructor() {
@@ -11,6 +14,20 @@ class App extends React.Component {
         cats: [],
         birds: []
      }
+  }
+
+  componentDidMount() {
+    Promise.all( [ fetchData(quotesEndpoint), fetchData(animalEndpoint("shibes")), fetchData(animalEndpoint("cats")), fetchData(animalEndpoint("birds"))])
+    .then(data => {
+      const quotesList = data[0];
+      const shibesList = data[1];
+      const catsList = data[2]
+      const birdsList = data[3];
+      this.setState({quotes: quotesList})
+      this.setState({shibes: shibesList})
+      this.setState({cats: catsList})
+      this.setState({birds: birdsList})
+    })
   }
 
   render() {
