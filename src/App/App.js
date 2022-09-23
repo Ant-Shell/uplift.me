dccab93c88d5150b4ac2a3ff2ebe602158b43e2b
Anthony Shellman <atshellman@gmail.com>
2022-09-23 16:43:26 -0600

    Change variable declaration

--- src/App/App.js
+++ src/App/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     Promise.all( [ fetchData(quotesEndpoint), fetchData(animalEndpoint("shibes")), fetchData(animalEndpoint("cats")), fetchData(animalEndpoint("birds"))])
     .then(data => {
       const quotesList = data[0];
-      let random = Math.floor(Math.random() * quotesList.length)
+      const random = Math.floor(Math.random() * quotesList.length)
       const shibesList = data[1];
       const catsList = data[2];
       const birdsList = data[3];
