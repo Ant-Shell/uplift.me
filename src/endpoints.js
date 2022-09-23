00f1aa51189eb12f46e3a5833cb1a2e86e7b62db
Anthony Shellman <atshellman@gmail.com>
2022-09-22 20:32:30 -0600

    Fix endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,4 @@
 const quotesEndpoint =  'https://type.fit/api/quotes'
-const animalEndpoint = (animal) => `https://shibe.online/api/${animal}?count=[20]`
+const animalEndpoint = (animal) => `https://shibe.online/api/${animal}?count=20`
 
 export {quotesEndpoint, animalEndpoint}
