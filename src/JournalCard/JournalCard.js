225c696eb914a97f28c9b9ecdcfc0676fe6e158d
Anthony Shellman <atshellman@gmail.com>
2022-09-24 10:50:35 -0600

    Add props and return

--- src/JournalCard/JournalCard.js
+++ src/JournalCard/JournalCard.js
@@ -1,12 +1,18 @@
 import React from "react"
 import "./JournalCard.css"
 
-const JournalCard = () => {
+const JournalCard = ( {id, date, entry}) => {
   // Make and return the journal card structure
   // ID
   // Date posted
   // Journal entry - how to handle this? How difficult would an onclick Modal be?
   // Button to remove entry?
+  return (
+    <div className="card">
+      <p>{date}</p>
+      <p>{entry}</p>
+    </div>
+  )
 }
 
 export default JournalCard
