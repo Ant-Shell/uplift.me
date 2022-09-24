0a9c15b473ef35cb82d729473d997c1d2381e9fe
Anthony Shellman <atshellman@gmail.com>
2022-09-24 14:49:00 -0600

    Add props, update formatting

--- src/JournalCard/JournalCard.js
+++ src/JournalCard/JournalCard.js
@@ -1,15 +1,12 @@
 import React from "react"
 import "./JournalCard.css"
 
-const JournalCard = ( {id, date, entry}) => {
-  // Make and return the journal card structure
-  // ID
-  // Date posted
+const JournalCard = ( {id, date, entry, time}) => {
   // Journal entry - how to handle this? How difficult would an onclick Modal be?
   // Button to remove entry?
   return (
     <div className="card">
-      <p>{date}</p>
+      <p>{date} @ {time}</p>
       <p>{entry}</p>
     </div>
   )
