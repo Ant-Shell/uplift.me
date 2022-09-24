16d552509359fabef29e1feb71e9e20658d638d1
Anthony Shellman <atshellman@gmail.com>
2022-09-23 20:02:05 -0600

    Add import and notes

--- src/JournalList/JournalList.js
+++ src/JournalList/JournalList.js
@@ -1,8 +1,10 @@
 import React from "react"
 import "./JournalList.css"
+import JournalCard from "../JournalCard/JournalCard"
 
 const JournalList = () => {
-  
+  // First, journalCards map function and make <JournalCard /> out of it and return
+  // Then return a div which contains { journalCards }
 }
 
 export default JournalList
