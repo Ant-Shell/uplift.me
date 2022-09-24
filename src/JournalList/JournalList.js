f206f7fc7a3c105079de12016501c573ae661c86
Anthony Shellman <atshellman@gmail.com>
2022-09-24 10:51:19 -0600

    Add props and return

--- src/JournalList/JournalList.js
+++ src/JournalList/JournalList.js
@@ -2,9 +2,24 @@ import React from "react"
 import "./JournalList.css"
 import JournalCard from "../JournalCard/JournalCard"
 
-const JournalList = () => {
+const JournalList = ( { journalList }) => {
   // First, journalCards map function and make <JournalCard /> out of it and return
+  const journalCards = journalList.map(journal => {
+    return (
+      <JournalCard 
+      id={journal.id}
+      date={journal.date}
+      entry={journal.journalEntry}
+      />
+    )
+  })
+
   // Then return a div which contains { journalCards }
+  return (
+    <div className="journallist-container">
+      { journalCards.length ? journalCards : <p className="add-journal-message">Please save a jounal</p>}
+    </div>
+  )
 }
 
 export default JournalList
