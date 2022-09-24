85f8034539854901196ea126588c6794365f51f6
Anthony Shellman <atshellman@gmail.com>
2022-09-24 16:47:03 -0600

    Add import, contruct function

--- src/AnimalSection/AnimalSection.js
+++ src/AnimalSection/AnimalSection.js
@@ -1,9 +1,14 @@
 import React from "react"
 import "./AnimalSection.css"
+import Animals from "../Animals/Animals"
 
 
-const AnimalSection = () => {
-
+const AnimalSection = ( {dogs} ) => {
+  return (
+    <div className="animals-section">
+      <Animals dogs={dogs}/>
+    </div>
+  )
 }
 
 export default AnimalSection
