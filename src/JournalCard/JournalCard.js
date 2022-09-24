2f32a159a583028bbda20f503ea138f4bc40b91f
Anthony Shellman <atshellman@gmail.com>
2022-09-23 20:02:51 -0600

    Initial commit

---
+++ src/JournalCard/JournalCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./JournalCard.css"
+
+const JournalCard = () => {
+  // Make and return the journal card structure
+  // ID
+  // Date posted
+  // Journal entry - how to handle this? How difficult would an onclick Modal be?
+  // Button to remove entry?
+}
+
+export default JournalCard
