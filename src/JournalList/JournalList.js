fd8df92edbd6d2c6fd1f7688812d4c3dcfa62c03
Anthony Shellman <atshellman@gmail.com>
2022-09-24 14:50:10 -0600

    Add more values, update message

--- src/JournalList/JournalList.js
+++ src/JournalList/JournalList.js
@@ -3,21 +3,21 @@ import "./JournalList.css"
 import JournalCard from "../JournalCard/JournalCard"
 
 const JournalList = ( { journalList }) => {
-  // First, journalCards map function and make <JournalCard /> out of it and return
   const journalCards = journalList.map(journal => {
     return (
       <JournalCard 
       id={journal.id}
       date={journal.date}
+      time={journal.time}
       entry={journal.journalEntry}
+      key={journal.id}
       />
     )
   })
 
-  // Then return a div which contains { journalCards }
   return (
     <div className="journallist-container">
-      { journalCards.length ? journalCards : <p className="add-journal-message">Please save a jounal</p>}
+      { journalCards.length ? journalCards : <p className="add-journal-message">Please save a journal entry</p>}
     </div>
   )
 }
