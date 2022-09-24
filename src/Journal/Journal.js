72e1225feb9889fce2179d44dd3728360c35db6d
Anthony Shellman <atshellman@gmail.com>
2022-09-23 19:42:44 -0600

    Add methods for form handling

--- src/Journal/Journal.js
+++ src/Journal/Journal.js
@@ -13,6 +13,20 @@ class Journal extends React.Component {
     this.setState({ [event.target.name]: event.target.value})
   }
 
+  submitJournalEntry = (event) => {
+    event.preventDefault()
+    const newJournalEntry = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addJournalEntry(newJournalEntry)
+    this.clearJournalInput()
+  }
+
+  clearJournalInput = () => {
+    this.setState({journalEntry: ""})
+  }
+
   render() {
     return (
       <form>
@@ -21,10 +35,10 @@ class Journal extends React.Component {
         placeholder="Journal"
         name="journal"
         value={this.state.journalEntry}
-        onChange={event => this.handleChange(event)}
+        onChange={(event => this.handleChange(event))}
         />
-        <button className="clear-button">Clear Entry</button>
-        <button className="add-button">Add Entry</button>
+        <button className="clear-button" onClick={() => this.clearJournalInput()}>Clear Entry</button>
+        <button className="add-button" onClick={(event) => this.submitJournalEntry(event)}>Add Entry</button>
       </form>
     )
   }
