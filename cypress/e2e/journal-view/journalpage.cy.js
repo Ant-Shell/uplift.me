3f62c0470fc746003d9e2307d478f3c414ff3d0a
Anthony Shellman <atshellman@gmail.com>
2022-09-25 09:52:31 -0600

    Add beforeEach, add initial tests with comments

---
+++ cypress/e2e/journal-view/journalpage.cy.js
@@ -0,0 +1,51 @@
+describe('Journal view', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('displays a header', () => {
+    // Logo/branding check
+    // View button check
+  })
+
+  it('displays a randomized quote', () => {
+    // Thought for the day header check
+    // Randomized quote check
+  })
+
+  it('displays a message if randomized quote is unavailable', () => {
+    // Thought for the day header check
+    // Randomized message unavailable check - need to build this in
+  })
+
+  it('should have a message input field', () => {
+    // Textarea check (add id?)
+  })
+
+  it('should have a Clear Entry button', () => {
+    // Button check (add id?)
+  })
+
+  it('should have an Add Entry button', () => {
+    // Button check (add id?)
+  })
+
+  it('should have a saved journals area', () => {
+    // Container check
+  })
+
+  it('should be able to clear a journal entry', () => {
+    // Verify message input is empty
+    // Add info in message input field
+    // Click 'Clear Entry' button
+    // Verify message input is empty
+  })
+
+  it('should be able to add a journal entry', () => {
+    // Verify message input is empty
+    // Add info in message input field
+    // Click 'Add Entry' button
+    // Verify message input is empty
+    // Verify journal entry is in saved journals area
+  })
+})
