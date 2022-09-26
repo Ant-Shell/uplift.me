535b6a41815459027177fbb8621eb6d51c0d8805
Anthony Shellman <atshellman@gmail.com>
2022-09-25 19:26:42 -0600

    Add credits

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -12,6 +12,7 @@ const Error = () => {
       alt='shiba looking at you'
       className="error-doggo"
       />
+      <p>Photo by <a href="https://unsplash.com/@isthatbrock?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brock Wegner</a> on <a href="https://unsplash.com/s/photos/funny-shiba?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
       <Link to={'/'}><button className="home-please">Home Please</button></Link>
     </section>
   )
